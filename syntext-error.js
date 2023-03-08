122d1e40054e4d0734283ecc1e85259ef2e0f282
HumaionKobir01~ <humaionkobir341@gmail.com>
2023-03-08 18:57:02 +0600

    Added => Understand Reference errors and Type errors message

---
+++ syntext-error.js
@@ -0,0 +1,46 @@
+function isBigger(num){
+    if(num > 5){
+        return num * num;
+    }
+    else{
+        return "this is too much";
+    }
+}
+// SyntaxError: Unexpected token '}'
+// }
+
+// const result = isBigger(5);
+// console.log(result);
+
+
+
+
+// SyntaxError: Missing initializer in const declaration
+// const sum = a, b => {
+//     console.log(a, b);
+// }
+// sum(5, 6);
+
+
+
+// SyntaxError: Unexpected identifier
+// const mask = {
+//     name: 'elon',
+//     company: 'tesla'
+//     age: 45
+// }
+// console.log(mask);
+
+
+
+
+// SyntaxError: Unexpected number
+// const numbers = [2, 58, 8 6, 20, 41];
+// console.log(numbers);
+
+
+
+// SyntaxError: Unexpected identifier
+// for(let i = 0; i < arry.length i++){
+
+// }
